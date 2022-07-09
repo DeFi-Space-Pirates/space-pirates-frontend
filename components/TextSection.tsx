dc63e1a02f2c5a72794d80f802053c38cd7b449a
Matteo Leonesi <matteo.leonesi@studenti.unicam.it>
2022-07-09 11:23:27 +0200

    feat: footer, first part of the landing page

---
+++ components/TextSection.tsx
@@ -0,0 +1,21 @@
+
+const TextSection = () => {
+    return (
+        <div>
+            <h2 className="mb-6 text-4xl font-bold text-center text-white">
+                Space Pirates
+            </h2>
+            <h3 className="my-4 text-2xl text-white">
+                imaginary space world where DeFi pirates can interact with DeFi and an Nft Game section.
+            </h3>
+            <button
+                className="bg-white font-bold rounded-full mt-6 py-4 px-8 shadow-lg uppercase tracking-wider"
+            >
+                Buy Tokens
+            </button>
+        </div>
+
+    )
+}
+
+export default TextSection
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import { useEffect } from 'react'
 import { useAlert } from '../contexts/AlertContext'
 import { useTronWeb } from '../contexts/TronWebContext'
+import TextSection from '../components/TextSection'
 
 const Home: NextPage = () => {
   const { toggleAlert } = useAlert()
@@ -13,27 +14,33 @@ const Home: NextPage = () => {
   }, [connectTronLink])
 
   return (
-    <div className="h-screen">
-      <Head>
-        <title>Space Pirates</title>
-      </Head>
-      <button
-        className="btn"
-        onClick={() => toggleAlert('Test Alert show', 'success')}
-      >
-        Hello daisyUI
-      </button>
-      <button
-        className="btn btn-warning"
-        onClick={async () =>
-          console.log(
-            await tronWeb.trx.getAccount('TTSFjEG3Lu9WkHdp4JrWYhbGP6K1REqnGQ'),
-          )
-        }
-      >
-        address
-      </button>
+    // <div className="h-screen">
+    //   <Head>
+    //     <title>Space Pirates</title>
+    //   </Head>
+    //   <button
+    //     className="btn"
+    //     onClick={() => toggleAlert('Test Alert show', 'success')}
+    //   >
+    //     Hello daisyUI
+    //   </button>
+    //   <button
+    //     className="btn btn-warning"
+    //     onClick={async () =>
+    //       console.log(
+    //         await tronWeb.trx.getAccount('TTSFjEG3Lu9WkHdp4JrWYhbGP6K1REqnGQ'),
+    //       )
+    //     }
+    //   >
+    //     address
+    //   </button>
+    // </div>
+    //    
+
+    <div className="container mx-auto m-10  px-6 text-center py-20 min-h-screen overflow-hidden bg-slate-900 rounded-lg ">
+      <TextSection />
     </div>
+
   )
 }
 
