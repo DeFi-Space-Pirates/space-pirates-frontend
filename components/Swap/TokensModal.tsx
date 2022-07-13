d0001e044f30f74b5ee026a78f39d525a86d8a95
yuripaoloni <yuri.paoloni3@gmail.com>
2022-07-13 15:30:15 +0200

    refactor: bg-base-100 on modal

--- components/Swap/TokensModal.tsx
+++ components/Swap/TokensModal.tsx
@@ -23,7 +23,7 @@ const TokensModal = ({
         readOnly
       />
       <div className="modal modal-bottom sm:modal-middle">
-        <div className="modal-box">
+        <div className="modal-box bg-base-100">
           <div className="flex justify-between">
             <h3 className="font-bold text-lg">Select token</h3>
             <button
