6fa32ba73784f50559e787b976bfc443f7efd2e5
yuripaoloni <yuri.paoloni3@gmail.com>
2022-07-13 16:31:58 +0200

    feat: /split first draft

--- pages/split/index.tsx
+++ pages/split/index.tsx
@@ -1,15 +1,80 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import DoubleArrows from '../../components/icons/DoubleArrows'
+import CardContainer from '../../components/layout/CardContainer'
 import Layout from '../../components/layout/Layout'
 import TradeTab from '../../components/layout/TradeTab'
 import { NextPageWithLayout } from '../_app'
 
 const Split: NextPageWithLayout = () => {
   return (
-    <div className="min-h-full">
+    <div className="h-screen flex justify-center items-center">
       <Head>
         <title>Space Pirates Split</title>
       </Head>
-      <p>ciao</p>
+      <CardContainer
+        title="Split"
+        subtitle="Split your Asteroids into stk-Asteroids and ve-Asteroids"
+      >
+        <div className="input-group justify-center">
+          <span className="gap-2">
+            <Image
+              src="https://s2.coinmarketcap.com/static/img/coins/64x64/1.png"
+              alt="token"
+              height={25}
+              width={25}
+            />
+            BTC
+          </span>
+          <input
+            type="text"
+            placeholder="0.0"
+            className="input input-lg w-3/4 shadow-md"
+          />
+        </div>
+        <div className="flex justify-center">
+          <button
+            className="btn btn-circle btn-outline border-0 my-4"
+            // onClick={() => invertTokens()}
+          >
+            <DoubleArrows />
+          </button>
+        </div>
+        <div className="flex justify-between gap-3">
+          <div className="input-group">
+            <span className="gap-2">
+              <Image
+                src="https://s2.coinmarketcap.com/static/img/coins/64x64/1.png"
+                alt="token"
+                height={25}
+                width={25}
+              />
+              BTC
+            </span>
+            <input
+              type="text"
+              placeholder="0.0"
+              className="input input-lg w-fit shadow-md"
+            />
+          </div>
+          <div className="input-group">
+            <span className="gap-2">
+              <Image
+                src="https://s2.coinmarketcap.com/static/img/coins/64x64/1.png"
+                alt="token"
+                height={25}
+                width={25}
+              />
+              BTC
+            </span>
+            <input
+              type="text"
+              placeholder="0.0"
+              className="input input-lg w-fit shadow-md"
+            />
+          </div>
+        </div>
+      </CardContainer>
     </div>
   )
 }
