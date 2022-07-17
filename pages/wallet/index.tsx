ac586b3d5b2723a9ec9f73a749bbfed40789a3af
yuripaoloni <yuri.paoloni3@gmail.com>
2022-07-17 12:29:09 +0200

    feat: wallet page link

---
+++ pages/wallet/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Wallet: NextPage = () => {
+  return (
+    <div className="min-h-full p-5 ">
+      <Head>Space Pirates Wallet</Head>
+    </div>
+  )
+}
+
+export default Wallet
