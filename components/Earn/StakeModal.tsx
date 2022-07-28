d241979bb8423118bdd06bb98d448c64c86b39a3
yuripaoloni <yuri.paoloni3@gmail.com>
2022-07-28 16:50:45 +0200

    feat: pools info, user info, stake

--- components/Earn/StakeModal.tsx
+++ components/Earn/StakeModal.tsx
@@ -1,15 +1,24 @@
 import Image from 'next/image'
 import { useState } from 'react'
 import { getTokenById } from '../../lib/tokens'
+import { StakeModalData } from '../../typings/Staking'
+import { Token1155 } from '../../typings/Token'
 import LoadingButton from '../layout/LoadingButton'
 import TokenInput from '../Trade/TokenInput'
 
 type StakeModalProps = {
   showModal: boolean
   handleShowModal: () => void
+  modalData: StakeModalData
+  loading: boolean
 }
 
-const StakeModal = ({ showModal, handleShowModal }: StakeModalProps) => {
+const StakeModal = ({
+  showModal,
+  handleShowModal,
+  modalData: { text, token, onSubmit },
+  loading,
+}: StakeModalProps) => {
   const [amount, setAmount] = useState('')
 
   const handleAmountChange = (amount: string) => {
@@ -36,16 +45,18 @@ const StakeModal = ({ showModal, handleShowModal }: StakeModalProps) => {
           }}
         >
           <div className="flex flex-col gap-y-4">
-            <h3 className="font-bold text-xl text-center">Stake DBL</h3>
+            <h3 className="font-bold text-xl text-center">
+              {text} {token?.symbol}
+            </h3>
             <TokenInput
               amount={amount}
               handleAmountChange={handleAmountChange}
-              token={getTokenById(1)!}
+              token={token}
             />
             <LoadingButton
-              loading={false}
-              onClick={() => {}}
-              text="Stake DBL"
+              loading={loading}
+              onClick={() => onSubmit(amount)}
+              text={`${text} ${amount} ${token?.symbol}`}
             />
           </div>
         </div>
--- typings/Staking.ts
+++ typings/Staking.ts
@@ -1,3 +1,5 @@
+import { Token1155 } from './Token'
+
 export type StakingPool = {
   exists: boolean
   rewardTokenId: number
@@ -7,4 +9,16 @@ export type StakingPool = {
   lastUpdateTime: number
   totalSupply: number
   accRewardPerShare: number
+}
+
+export type UserInfo = {
+  balances: string
+  rewardDebt: string
+  rewards: string
+}
+
+export type StakeModalData = {
+  text: string
+  token: Token1155
+  onSubmit: (amount: string) => void
 }
