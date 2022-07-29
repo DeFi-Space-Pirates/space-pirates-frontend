5f1dfe3dd87d8293ce19477a5ea227f4c2f30bed
yuripaoloni <yuri.paoloni3@gmail.com>
2022-07-29 14:08:13 +0200

    feat: implement tronweb for the swap

--- pages/swap/index.tsx
+++ pages/swap/index.tsx
@@ -12,19 +12,28 @@ import LoadingButton from '../../components/layout/LoadingButton'
 
 import { Token } from '../../typings/Token'
 import { useAlert } from '../../contexts/AlertContext'
+import { useTronWeb } from '../../contexts/TronWebContext'
 import tokensList from '../../config/constants/dexTokensList.json'
+import SpacePiratesFactory from '../../config/artifacts/SpacePiratesFactory.json'
+import SpacePiratesRouter from '../../config/artifacts/SpacePiratesRouter.json'
+import StaticRouterAbi from '../../config/artifacts/StaticRouterAbi.json'
+import { addresses } from '../../config/addresses'
+import { convertToHex, convertToNumber, NULL_ADDRESS } from '../../lib/tronweb'
 
 const Swap: NextPageWithLayout = () => {
   const [tokenA, setTokenA] = useState<Token>(tokensList.tokens[0])
   const [tokenB, setTokenB] = useState<Token>(tokensList.tokens[1])
   const [amountA, setAmountA] = useState('0.0')
   const [amountB, setAmountB] = useState('0.0')
+  const [loadingA, setLoadingA] = useState(false)
+  const [loadingB, setLoadingB] = useState(false)
 
   const [isTokenA, setIsTokenA] = useState(false)
   const [showModal, setShowModal] = useState(false)
 
   const [loading, setLoading] = useState(false)
 
+  const { tronWeb, address } = useTronWeb()
   const { toggleAlert } = useAlert()
 
   const handleShowModal = (isTokenA?: boolean) => {
@@ -32,27 +41,76 @@ const Swap: NextPageWithLayout = () => {
     isTokenA !== undefined && setIsTokenA(isTokenA)
   }
 
-  const handleTokenChange = (token: Token) => {
+  const handleTokenChange = async (token: Token) => {
     if (isTokenA) {
-      if (token === tokenB) {
-        setTokenB(tokenA)
+      const valid = await isValidPair(token, tokenB)
+      if (valid) {
+        if (token === tokenB) {
+          setTokenB(tokenA)
+        }
+
+        setTokenA(token)
       }
-      setTokenA(token)
     } else {
-      if (token === tokenA) {
-        setTokenA(tokenB)
+      const valid = await isValidPair(tokenA, token)
+      if (valid) {
+        if (token === tokenA) {
+          setTokenA(tokenB)
+        }
+        setTokenB(token)
       }
-      setTokenB(token)
     }
+
     setShowModal(false)
   }
 
-  const handleAmountAChange = (amount: string) => {
-    setAmountA(amount)
+  const isValidPair = async (
+    tokenA: Token,
+    tokenB: Token,
+  ): Promise<boolean> => {
+    try {
+      const spacePiratesFactory = await tronWeb.contract(
+        SpacePiratesFactory.abi,
+        addresses.shasta.factoryContract,
+      )
+
+      const pair = await spacePiratesFactory
+        .getPair(tokenA.id, tokenB.id)
+        .call()
+
+      if (pair === NULL_ADDRESS) {
+        toggleAlert('Invalid pair. Try a different combination.', 'error')
+        return false
+      }
+
+      return true
+    } catch (error) {
+      toggleAlert('Invalid pair. Try a different combination.', 'error')
+      return false
+    }
   }
 
-  const handleAmountBChange = (amount: string) => {
+  const handleAmountAChange = async (amount: string) => {
+    setAmountA(amount)
+
+    setLoadingB(true)
+
+    setTimeout(async () => {
+      const amountB = await getExpectedOutput(amount)
+      setAmountB('amountB')
+      setLoadingB(false)
+    }, 2000)
+  }
+
+  const handleAmountBChange = async (amount: string) => {
     setAmountB(amount)
+    setLoadingA(true)
+
+    setTimeout(async () => {
+      const amountA = await getExpectedOutput(amount)
+      setAmountA(amountA)
+      setLoadingA(false)
+    }, 2000)
   }
 
   const invertTokens = () => {
@@ -62,14 +120,57 @@ const Swap: NextPageWithLayout = () => {
     setAmountB(amountA)
   }
 
+  //TODO: try again after liquidity added to the pool
+  const getExpectedOutput = async (amount: string): Promise<string> => {
+    try {
+      const spacePiratesRouterStatic = await tronWeb.contract(
+        StaticRouterAbi,
+        addresses.shasta.routerContract,
+      )
+
+      const res = await spacePiratesRouterStatic
+        .swapExactTokensForTokens(
+          convertToHex(amount, 1e18),
+          convertToHex(amount, 0.98 * 1e18),
+          [tokenA.id, tokenB.id],
+          address,
+          Math.floor((new Date().getTime() + 300) / 1000), //300 is 5 minutes
+        )
+        .call()
+
+      console.log(res)
+
+      return convertToNumber(res._hex, 1e18)
+    } catch (err) {
+      console.log(err)
+      toggleAlert('Cannot calculate the output. Try again.', 'error')
+      return '0.0'
+    }
+  }
+
+  //TODO: try again after liquidity added to the pool
   const onSwapTokens = async () => {
     setLoading(true)
 
-    //TODO validate tokens balance
-
     try {
-      //TODO implement tronweb swap logic
+      const spacePiratesRouter = await tronWeb.contract(
+        SpacePiratesRouter.abi,
+        addresses.shasta.routerContract,
+      )
+
+      const res = await spacePiratesRouter
+        .swapExactTokensForTokens(
+          convertToHex(amountA, 1e18),
+          convertToHex(amountA, 0.98 * 1e18),
+          [tokenA.id, tokenB.id],
+          address,
+          Math.floor((new Date().getTime() + 300) / 1000), //300 is 5 minutes
+        )
+        .send()
+
+      console.log(res)
     } catch (err) {
+      console.log(err)
       toggleAlert('Error during the swap. Try again', 'error')
     } finally {
       setLoading(false)
@@ -96,6 +197,7 @@ const Swap: NextPageWithLayout = () => {
           amount={amountA}
           handleAmountChange={handleAmountAChange}
           token={tokenA}
+          loading={loadingA}
         />
         <div className="flex justify-center">
           <button
@@ -110,13 +212,15 @@ const Swap: NextPageWithLayout = () => {
           amount={amountB}
           handleAmountChange={handleAmountBChange}
           token={tokenB}
+          loading={loadingB}
         />
-        <div className="mt-8">
+        <div className="mt-8 text-center">
           <LoadingButton
             text="SWAP"
             loading={loading}
             onClick={() => onSwapTokens()}
           />
+          <small>Default slippage: 2%</small>
         </div>
       </CardContainer>
     </div>
